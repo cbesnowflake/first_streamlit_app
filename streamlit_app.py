ecd3733d5aa346089b5e8a3c213e3654baaa2e0a
cbesnowflake <118808935+cbesnowflake@users.noreply.github.com>
2022-11-22 11:00:35 +0100

    Update streamlit_app.py
    Updated breakfast menu header

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('🥣Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
-streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
+streamlit.text('🥣Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado toasts')
